cb4985dfcbe333e7ad30915dedb7c3a57ddcad0e
Steve Hayes <steven0525@aim.com>
2026-02-27 20:04:37 -0500

    chore: stop tracking node_modules/.firebase/backups; add gitignore

--- functions/src_backup_20260225_164222/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Cloud Functions entrypoint.
- * Important: do NOT call initializeApp() here. Admin init is centralized in ./admin.
- */
-export * from "./provision";
-export * from "./scheduler";
-export * from "./setUserRole";
-export * from "./v9/callables";
-
